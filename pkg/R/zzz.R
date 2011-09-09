aca3c7d8452fc849d423b99e7ac3c41076c20d6d
yneol <yneol@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-09-09 08:15:23 +0000

    zzz simplified
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r2stats@6 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/zzz.R
+++ pkg/R/zzz.R
@@ -1,9 +1,3 @@
 .First.lib = function(...) {
-  cat("\n--------------------------------")
-  cat("\nR2STATS : chargement en cours...")
-  cat("\n--------------------------------\n")
   R2STATS()
-  cat("\n--------------------------------")
-  cat("\nR2STATS : chargement terminé.")
-  cat("\n--------------------------------\n")
 }

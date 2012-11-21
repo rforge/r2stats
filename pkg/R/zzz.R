c3cbca8a0edf047a497c40add9b5a1b0e9ef0a25
yneol <yneol@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-11-21 14:57:19 +0000

    gtkrc file creation added.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r2stats@33 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/zzz.R
+++ pkg/R/zzz.R
@@ -6,6 +6,14 @@
   # A first dataset
   data(sleepstudy,package="lme4")
 
+  # Create gtkrc file and select Windows theme if necessary
+  # (Default Gnome theme is ugly under Win
+  if(.Platform$OS.type == "windows") {
+    rc.path = paste(Sys.getenv("R_HOME"),"library/RGtk2/gtk/i386/etc/gtk-2.0",sep="/")
+    rc.file = paste(rc.path,"gtkrc",sep="/")
+    if(file.exists(rc.path) && !file.exists(rc.file)) writeLines("gtk-theme-name=\"MS-Windows\"",rc.file)
+  }
+
   # build the GUI
   R2STATS()
 }
